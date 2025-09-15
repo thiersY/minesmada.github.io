4d91c8ea3438f8c8b6a13b6f52f97587cce0ac78
thiersY <71712447+thiersY@users.noreply.github.com>
2025-09-15 23:54:35 +0300

    Update pv_script.php

--- pv_script.php
+++ pv_script.php
@@ -88,7 +88,7 @@
         }
 
         function renderPdf() {
-            const pdfUrl = 'http://minesmada.ddns.net/documents/pv_de_controle.pdf';
+            const pdfUrl = '/documents/pv_de_controle.pdf';
             const pdfjsLib = window['pdfjs-dist/build/pdf'];
             pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
 
